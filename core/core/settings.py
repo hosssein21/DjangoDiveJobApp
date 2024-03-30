57dcd762a62b57e81e6bda107bb09e406337cc75
hosssein <hossseinkazemi79@gmail.com>
2024-03-30 16:07:33 +0330

    adding template of website app

--- core/core/settings.py
+++ core/core/settings.py
@@ -115,13 +115,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'staticfiles/'
+STATIC_URL = '/static/'
 
-MEDIA_URL = 'media/'
-MEDIA_URL = BASE_DIR/'media/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
-STATICFILES_DIRS=[BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
